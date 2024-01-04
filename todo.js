1442af40e236cfe2c92e547e8cdee92655755e38
sanjana <21pa1a12b0@vishnu.edu.in>
2024-01-04 23:42:59 +0530

    initial commit

---
+++ todo.js
@@ -0,0 +1,29 @@
+const todoList = require('../index')(); 
+
+const { all, markAsComplete, add } = todoList;
+
+describe("Todolist Test Suites", () => {
+  beforeAll(() => {
+    add({
+      title: "Test todo",
+      completed: false,
+      dueDate: new Date().toLocaleDateString("en-CA")
+    });
+  });
+
+  test("Should add new todo", () => {
+    const todoItemCount = all.length;
+    add({
+      title: "Test todo",
+      completed: false,
+      dueDate: new Date().toLocaleDateString("en-CA")
+    });
+    expect(all.length).toBe(todoItemCount + 1);
+  });
+
+  test("Should mark a todo as complete", () => {
+    expect(all[0].completed).toBe(false);
+    markAsComplete(0);
+    expect(all[0].completed).toBe(true);
+  });
+});
